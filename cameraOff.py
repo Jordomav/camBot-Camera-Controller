b604f9f73d60f99af936f6a9c03330e9a5b9dfd6
Jordomav <jordomav@gmail.com>
2016-09-26 13:07:35 -0700

    init commit

---
+++ cameraOff.py
@@ -0,0 +1,5 @@
+from picamera import PiCamera
+
+camera = PiCamera()
+
+camera.stop_preview()
---
+++ takeSnap.py
@@ -0,0 +1,12 @@
+from picamera import PiCamera
+from time import sleep
+from random import randint
+
+camera = PiCamera()
+
+randomNumber = randint(0,999)
+
+camera.start_preview()
+sleep(5)
+camera.capture('/home/pi/Desktop/' + randomNumber + '.jpg')
+camera.stop_preview()
